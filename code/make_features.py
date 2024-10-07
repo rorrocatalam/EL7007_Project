ef9c2621273245c0fd6ccdd6c43119c5cbce00b5
rorrocatalam <rodrigo.catalan.b@ug.uchile.cl>
2024-10-07 11:30:35 -0300

    Agrego código para extraer features con redes preentrenadas y resultados de las mismas en archivos csv

---
+++ code/make_features.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+from feature_extractor import *
+
+db_foldername = 'database'
+ft_foldername = 'features'
+
+# CSV files to write
+vgg16_csv     = os.path.join(ft_foldername, 'vgg16.csv')
+resnet_csv    = os.path.join(ft_foldername, 'resnet.csv')
+inception_csv = os.path.join(ft_foldername, 'inception.csv')
+mobilenet_csv = os.path.join(ft_foldername, 'mobilenet.csv')
+
+def save_features(csv_file, subject, features):
+    features_with_subject = np.insert(features, 0, subject)
+    with open(csv_file, 'a') as f:
+        np.savetxt(f, [features_with_subject], delimiter=',', fmt='%.6f')
+        
+for root, dirs, files in os.walk(db_foldername):
+    for file in files:
+        if file.endswith('.jpg'):
+            # Image path
+            img_path = os.path.join(root, file)
+            # Relative path of the image
+            img_relpath = os.path.relpath(img_path)
+            
+            print(img_relpath)
+            # # Subject
+            # subject = int(os.path.split(os.path.dirname(img_relpath))[-1])
+            
+            # # Feature extractor
+            # f_vgg16     = feature_extractor_VGG16(img_relpath)
+            # f_resnet    = feature_extractor_ResNet50(img_path)
+            # f_inception = feature_extractor_InceptionV3(img_relpath)
+            # f_mobilenet = feature_extractor_MobileNet(img_relpath)
+            
+            # # Save features to CSV files
+            # save_features(vgg16_csv, subject, f_vgg16)
+            # save_features(resnet_csv, subject, f_resnet)
+            # save_features(inception_csv, subject, f_inception)
+            # save_features(mobilenet_csv, subject, f_mobilenet)
+            
