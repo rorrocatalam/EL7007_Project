debec0aa7059d758a9865a83ad58a8fa1db09304
Matias Urrea <urreamatias@gmail.com>
2024-11-21 20:37:56 -0300

    Se agrega la función print_layers y sus resultados

--- code/feature_extractor.py
+++ code/feature_extractor.py
@@ -2,7 +2,9 @@
 #  Imports
 # -----------------------------------------------------------------------------
 import numpy as np
-
+import os
+import matplotlib.pyplot as plt
+from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications import VGG16, ResNet50, InceptionV3, MobileNet
@@ -10,6 +12,9 @@ from tensorflow.keras.applications.vgg16 import preprocess_input as ppi_vgg16
 from tensorflow.keras.applications.resnet50 import preprocess_input as ppi_resnet50
 from tensorflow.keras.applications.inception_v3 import preprocess_input as ppi_inception
 from tensorflow.keras.applications.mobilenet import preprocess_input as ppi_mobilenet
+from tensorflow.keras import layers
+
+
 
 # -----------------------------------------------------------------------------
 # VGG16
@@ -90,3 +95,71 @@ def feature_extractor_MobileNet(path):
     img_rgb = np.expand_dims(img_rgb, axis=0)
     img_rgb = ppi_mobilenet(img_rgb)
     return model_mobilenet.predict(img_rgb)[0][0,0,:]
+
+
+
+
+# -----------------------------------------------------------------------------
+# Layer Reader
+# -----------------------------------------------------------------------------
+def print_layers(n_model, img_tensor):
+    global model_inception
+    global model_mobilenet
+    global model_resnet50
+    global model_vgg16
+    names=models  = ['inception', 'mobilenet', 'resnet50','vgg16']
+    models  = [model_inception, model_mobilenet, model_resnet50, model_vgg16]
+    model = models[n_model]
+    layer_outputs = []
+    layer_names = []
+    for layer in model.layers:
+        if isinstance(layer, (layers.Conv2D, layers.MaxPooling2D)):
+            layer_outputs.append(layer.output)
+            layer_names.append(layer.name)
+    activation_model = keras.Model(inputs=model.input, outputs=layer_outputs)
+    activations = activation_model.predict(img_tensor)   
+    images_per_row = 16
+    for layer_name, layer_activation in zip(layer_names, activations):
+        n_features = layer_activation.shape[-1]
+        size = layer_activation.shape[1]
+        n_cols = n_features // images_per_row
+        display_grid = np.zeros(((size + 1) * n_cols - 1,
+                                images_per_row * (size + 1) - 1))
+        for col in range(n_cols):
+            for row in range(images_per_row):
+                channel_index = col * images_per_row + row
+                channel_image = layer_activation[0, :, :, channel_index].copy()
+                if channel_image.sum() != 0:
+                    channel_image -= channel_image.mean()
+                    channel_image /= channel_image.std()
+                    channel_image *= 64
+                    channel_image += 128
+                channel_image = np.clip(channel_image, 0, 255).astype("uint8")
+                display_grid[
+                    col * (size + 1): (col + 1) * size + col,
+                    row * (size + 1) : (row + 1) * size + row] = channel_image
+        scale = 1. / size
+        plt.figure(figsize=(scale * display_grid.shape[1],
+                            scale * display_grid.shape[0]))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.axis("off")
+        plt.imshow(display_grid, aspect="auto", cmap="viridis")
+        os.makedirs(f'HiddenLayers/{names[n_model]}', exist_ok=True)
+        plt.savefig(f'HiddenLayers/{names[n_model]}/{layer_name}')
+        plt.close()
+
+img_path = 'database/1/001_1_1.jpg'
+img = image.load_img(img_path, color_mode='grayscale', target_size=(224, 224))
+img_array = image.img_to_array(img)
+img_rgb = np.repeat(img_array, 3, axis=2)
+img_rgb = np.expand_dims(img_rgb, axis=0)
+print_layers(1, img_rgb)
+print_layers(2, img_rgb)
+print_layers(3, img_rgb)
+
+img = image.load_img(img_path, color_mode='grayscale', target_size=(299, 299))
+img_array = image.img_to_array(img)
+img_rgb = np.repeat(img_array, 3, axis=2)
+img_rgb = np.expand_dims(img_rgb, axis=0)
+print_layers(0, img_rgb)
